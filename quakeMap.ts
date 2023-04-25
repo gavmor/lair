8c3f68457ac14c8845387027ee70d7e6f56d2c92
Gavin Morgan <morgan.gavin@gmail.com>
2023-04-24 17:29:54 -0700

    extract quakes

---
+++ quakeMap.ts
@@ -0,0 +1,46 @@
+import * as blessed from "blessed";
+import * as contrib from "blessed-contrib";
+import { addMarkers as addQuakes } from "./quake";
+
+
+const createScreen = (title: string) => blessed.screen({
+  smartCSR: true,
+  title,
+  autoPadding: true,
+  dockBorders: true,
+  fullUnicode: true,
+});
+
+const createMap = (label: string) => contrib.map({
+  // @ts-expect-error
+  label,
+  startLon: 0,
+  startLat: 0,
+  width: "100%",
+  height: "100%",
+});
+
+const MINUTE_IN_MILLISECONDS = 60 * 1000;
+
+function main() {
+  const screen = createScreen("Where Does \"Title\" This Even Show Up?");
+  const map = createMap("Last Week in Earthquakes");
+  screen.append(map);
+
+  async function renderLoop() {
+    while (true) {
+      await addQuakes(map);
+      screen.render();
+      await new Promise((resolve) => setTimeout(resolve, MINUTE_IN_MILLISECONDS)); // Update every minute
+    }
+  }
+
+  renderLoop();
+
+  // Handle key events
+  screen.key(["escape", "q", "C-c"], () => {
+    return process.exit(0);
+  });
+}
+
+main()
