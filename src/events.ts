18bb50426596622630cd49adfe80969c4460d793
Gavin Morgan <morgan.gavin@gmail.com>
2023-04-26 17:18:20 -0700

    table

--- src/events.ts
+++ src/events.ts
@@ -1,14 +1,12 @@
-import { Widgets } from "blessed-contrib";
+import type { Widgets } from "blessed-contrib";
 import type { MapMarker } from "./lib/BlessedContrib";
 import type { PredictHQEvent } from "./lib/PredictHQ";
 import axios, { AxiosResponse } from 'axios';
-import * as crypto from "crypto";
 
 const env = process.env;
 const phqToken = env.PHQ_TOKEN;
 
 export async function getEvents(): Promise<AxiosResponse> {
-    const now = new Date();
     return await axios.get(
         'https://api.predicthq.com/v1/events/',
         {
@@ -18,9 +16,10 @@ export async function getEvents(): Promise<AxiosResponse> {
             },
             params: {
                 category: Object.keys(charByCategory).join(","),
-                rank_level: "5,4,3",
-                "active.gte": now.getDate() - 7,
-                "active.lte": new Date().getDate(),
+                rank_level: "5",
+                limit: 30,
+                // "active.gte": new Date().getDate() - 7,
+                // "active.lte": new Date().getDate(),
             }
         }
     )
@@ -30,28 +29,29 @@ export async function getEvents(): Promise<AxiosResponse> {
 const createMarker = ({ location, category }: PredictHQEvent): MapMarker => ({
     lat: location[1],
     lon: location[0],
-    color: category == "disasters" ? [255, 0, 0] : [255, 255, 0],
+    color: category == "disasters" || category == "terror" ? [255, 0, 0] : [255, 255, 0],
     char: charByCategory[category] || console.log(category) || "?"
 })
 
 
-export async function addMarkers(
+export async function getAndAddMarkers(
     mapElement: Widgets.MapElement,
     log
 ): Promise<Array<PredictHQEvent>> {
-    const { data: eventSet, request } = await getEvents();
-    const markers = eventSet.results.map(createMarker);
-    // eventSet.results.map(e => log.log(e.category))
-    // @ts-expect-error
-    markers.map(async marker => mapElement.addMarker(marker))
-    return eventSet.results
+    const { data: {results: events}} = await getEvents();
+    markEventsOnMap(events, mapElement);
 }
-const charByCategory = {
-    concerts: "&",
-    "performing-arts": "%",
-    disasters: "!",
-    sports: "@",
+export const charByCategory = {
+    // concerts: "&",
+    // "performing-arts": "%",
+    // disasters: "!",
+    terror: "!",
+    // sports: "@",
     community: "#",
-    conferences: "#",
-    expos: "#"
+    // conferences: "#",
+    // expos: "#",
 }
+export function markEventsOnMap(events: Array<PredictHQEvent>, mapElement: Widgets.MapElement) {
+    events.map(createMarker).forEach((marker) => mapElement.addMarker(marker));
+}
+
--- src/map.ts
+++ src/map.ts
@@ -1,7 +1,8 @@
 import * as blessed from "blessed";
 import * as contrib from "blessed-contrib";
 import { addMarkers as addQuakes } from "./quake";
-import { addMarkers as addEvents, getEvents } from "./events";
+import { getAndAddMarkers as addEvents, charByCategory, getEvents, markEventsOnMap } from "./events";
+import { PredictHQEvent } from "./lib/PredictHQ";
 
 const ONE_MINUTE = 60 * 1000;
 const GOLDEN = (1 + Math.sqrt(5)) / 2
@@ -23,15 +24,42 @@ const createMap = (label: string) => contrib.map({
   height: "100%",
 });
 
+function romanize(num) {
+  var lookup = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }, roman = '', i;
+  for (i in lookup) {
+    while (num >= lookup[i]) {
+      roman += i;
+      num -= lookup[i];
+    }
+  }
+  return roman;
+}
+
 async function hydrateLoop(
   map: contrib.Widgets.MapElement,
   table: contrib.Widgets.TableElement,
   log: contrib.Widgets.LogElement,
   screen: blessed.Widgets.Screen
 ) {
-  log.log("test")
-  getEvents().then(({data: {results: events}}) => events.forEach(({category}) => log.log(category)))
-  addEvents(map, log).then(() => screen.render())
+  getEvents()
+    .then(({ data: { results: events } }) => {
+      markEventsOnMap(events, map)
+      table.setData(
+        {
+          headers: ['', 'Day', 'Loc', 'Title']
+          , data: events.map((e: PredictHQEvent) => [
+            charByCategory[e.category],
+            new Date(e.start).toDateString().slice(0, 3),
+            e.country,
+            e.title,
+          ])
+
+        })
+    })
+    .finally(() => screen.render())
+  // .then(({data: {results: events}}) => events.forEach(({location}) => log.log(location[0].toString())))
+
+  // addEvents(map, log).then(() => screen.render())
   addQuakes(map).then(() => screen.render()).then(results => results.map(e => log.log(e.category)))
   await new Promise((resolve) => setTimeout(resolve, ONE_MINUTE * 60));
 
@@ -42,24 +70,19 @@ async function hydrateLoop(
 function main() {
   const screen = createScreen("Last Week on Earth"); // Terminal window title
   var grid = new contrib.grid({ rows: 12, cols: 12, screen })
-  var map = grid.set(0, 0, 10, 10, contrib.map, { label: screen.title })
+  var map = grid.set(0, 0, 10, 8, contrib.map, { label: screen.title })
   var log = grid.set(10, 0, 2, 12, contrib.log, { label: "Log" })
-  const table = grid.set(0, 10, 10, 2, contrib.table, {
+  const table = grid.set(0, 8, 10, 4, contrib.table, {
     keys: true
+    , fg: "white"
     , interactive: false
     , label: 'Events'
     , border: { type: "line" }
-    , columnSpacing: 4 //in chars
-    , columnWidth: [3, 3, 3] /*in chars*/
+    , columnSpacing: 2 //in chars
+    , columnWidth: [1, 3, 3, 35] /*in chars*/
   })
 
-  table.setData(
-    {
-      headers: ['col1', 'col2', 'col3']
-      , data:
-        [[1, 2, 3]
-          , [4, 5, 6]]
-    })
+
 
   // screen.append(map);
   // screen.append(log)
